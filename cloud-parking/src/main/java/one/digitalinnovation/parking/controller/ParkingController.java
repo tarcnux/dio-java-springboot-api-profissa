ae14e73a1a0204bd2dfc51b407f747d80f2ab123
tarcnux <github@tarcnux.com.br>
2022-10-19 17:11:31 -0300

    Add ParkingService

--- cloud-parking/src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
+++ cloud-parking/src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
@@ -8,35 +8,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import one.digitalinnovation.parking.model.Parking;
+import one.digitalinnovation.parking.service.ParkingService;
 
 @RestController
 @RequestMapping("/parking")
 public class ParkingController {
 	
+	
+	private final ParkingService parkingService;
+		
+	//Autowired is not recommended anymore
+	public ParkingController(ParkingService parkingService) {
+		this.parkingService = parkingService;
+	}
+
+
+
 	@GetMapping
 	public List<Parking> findAll() {
 		
 		System.out.println("ParkingController findAll()");
-		
-		var parking_1 = new Parking();
-		var parking_2 = new Parking();
-		
-		var parkingList = new ArrayList<Parking>();
-		
-		parking_1.setColor("PRETO");
-		parking_1.setLicense("MSS-1111");
-		parking_1.setModel("VW GOL");
-		parking_1.setState("SC");
-		
-		parking_2.setColor("AZUL");
-		parking_2.setLicense("MST-666");
-		parking_2.setModel("FORD KA");
-		parking_2.setState("MG");
-		
-		parkingList.add(parking_1);
-		parkingList.add(parking_2);
-		
-		return parkingList;
+
+		return parkingService.findAll();
 		
 	}
 
